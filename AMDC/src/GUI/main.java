3d133ee57b42f1d672d7e66d8013fb7c5ba5dce0
rhit-lopezmo <78335296+rhit-lopezmo@users.noreply.github.com>
2022-09-27 21:49:18 -0400

    Added IO for Easier Visual Rep.
    IO for each parameter of the random code gen; shows what the app will pull from the excel file to generate codes.

--- AMDC/src/GUI/main.java
+++ AMDC/src/GUI/main.java
@@ -1,10 +1,18 @@
 package GUI;
+import java.util.Scanner;
+
 import backendFeatures.BackendFeatures;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println(BackendFeatures.randomCodeGen(8, "Manuel Lopez", "100010"));
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please provide the name of this member: ");
+		String memName = sc.nextLine();
+		System.out.print("Please provide a code length: ");
+		String codeLength = sc.nextLine();
+		System.out.print("Please provide a member ID: ");
+		String memID = sc.nextLine();
+		System.out.println("One-Use Unique Code: " + BackendFeatures.randomCodeGen(Integer.parseInt(codeLength), memName, memID));
 	}
-
 }
